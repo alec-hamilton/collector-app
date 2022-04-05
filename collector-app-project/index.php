e666a285f5a92e8d17a4156c67e7f5ee435bfe15
alech <hamiltonalec@gmail.com>
2022-04-05 16:54:21 +0100

    added separate page to host form

--- collector-app-project/index.php
+++ collector-app-project/index.php
@@ -19,34 +19,11 @@ $dbdata = fetchAllReleaseData($pdo);
 </head>
 
 <body>
-    <nav class="nav">
-        <a class="menu-link" href="#">Alec's Records</a>
-    </nav>
+    <?php include "header.html"; ?>
 	<div class="release-container">
 		<?php displayReleases($dbdata); ?>
 	</div>
-    <div>
-        <form action="form-handling.php" method="post" enctype="multipart/form-data">
-            <label for="artistname">Artist</label>
-            <input type="text" id="artistname" name="artist" required><br>
-            <label for="releasename">Release name</label>
-            <input type="text" id="releasename" name="release" required><br>
-            <label for="labelname">Label</label>
-            <input type="text" id="labelname" name="label" required><br>
-            <label for="yearname">Year</label>
-            <input type="text" id="yearname" name="year" required><br>
-            <label for="format">Format</label>
-            <select id="format" name="format" required>
-                <option value="1">7"</option>
-                <option value="2">10"</option>
-                <option value="3">12"</option>
-                <option value="4">2x12"</option>
-            </select><br>
-            <label for="fileupload">Upload cover image</label>
-            <input type="file" id="fileupload" name="newFile" required><br>
-            <input type="submit" value="Submit">
-        </form>
-    </div>
+
 </body>
 
 </html>
