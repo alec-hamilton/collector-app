9e62b18907ec8de1b57f2f0851805f3eff0f64cb
alech <hamiltonalec@gmail.com>
2022-04-07 14:11:49 +0100

    updated sql added

--- collector-app-project/documents/collector-app_2022-04-07.sql
+++ collector-app-project/documents/collector-app_2022-04-07.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.37)
 # Database: collector-app
-# Generation Time: 2022-04-04 09:28:24 +0000
+# Generation Time: 2022-04-07 13:11:08 +0000
 # ************************************************************
 
 
@@ -29,7 +29,7 @@ CREATE TABLE `formats` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `format` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 LOCK TABLES `formats` WRITE;
 /*!40000 ALTER TABLE `formats` DISABLE KEYS */;
@@ -39,7 +39,8 @@ VALUES
 	(1,'7\"'),
 	(2,'10\"'),
 	(3,'12\"'),
-	(4,'2 x 12\"');
+	(4,'2 x 12\"'),
+	(5,'3 x 12\"');
 
 /*!40000 ALTER TABLE `formats` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -58,20 +59,26 @@ CREATE TABLE `releases` (
   `year` year(4) DEFAULT NULL,
   `format` int(11) unsigned DEFAULT NULL,
   `image_url` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`), 
-  CONSTRAINT `fk_releases_formats` FOREIGN KEY (`format`) REFERENCES `formats`(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  KEY `fk_releases_formats` (`format`),
+  CONSTRAINT `fk_releases_formats` FOREIGN KEY (`format`) REFERENCES `formats` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
 
 LOCK TABLES `releases` WRITE;
 /*!40000 ALTER TABLE `releases` DISABLE KEYS */;
 
 INSERT INTO `releases` (`id`, `artist`, `release_name`, `label`, `year`, `format`, `image_url`)
 VALUES
-	(1,'Florian T M Zeisig','Slow Bench','enmossed','2022',1,'https://www.discogs.com/release/22458883-Florian-T-M-Zeisig-Slow-Bench/image/SW1hZ2U6NzQ4MzQzMTE='),
-	(2,'Another Channel','Behind The Glow','Moonshine Recordings','2022',3,'https://www.discogs.com/release/22369501-Another-Channel-Behind-The-Glow/image/SW1hZ2U6NzQ0Njk5MTM='),
-	(3,'Ike','Stone Diviner','Die Orakel','2021',3,'https://www.discogs.com/release/19859281-Ike-Stone-Diviner/image/SW1hZ2U6NjQzMjk4NzE='),
-	(4,'Priori','Your Own Power','NAFF','2021',3,'https://www.discogs.com/release/20655262-Priori-Your-Own-Power/image/SW1hZ2U6Njg5NzA0MTg='),
-	(5,'Ura','Entertainment','Collect-Call','2019',3,'https://www.discogs.com/release/13339374-URA-Entertainment/image/SW1hZ2U6MzkwODk5MDE=');
+	(1,'Florian T M Zeisig','Slow Bench','enmossed','2022',1,'R-22458883-1646926153-9657.jpeg'),
+	(2,'Another Channel','Behind The Glow','Moonshine Recordings','2022',3,'a4095236088_16.jpeg'),
+	(3,'Ike','Stone Diviner','Die Orakel','2021',3,'R-19859281-1628951643-1229.jpeg'),
+	(4,'Priori','Your Own Power','NAFF','2021',3,'R-20655262-1637187743-8641.jpeg'),
+	(5,'Ura','Entertainment','Collect-Call','2019',3,'R-13339374-1552351934-1616.jpeg'),
+	(28,'Huerco S','Plonk','Insienso','2022',4,'ce8e33394ab7a83ac40db37c67119c3890687f76.jpg'),
+	(29,'Jake Muir','Mana','Illian Tape','2021',3,'0255fc73dae8204c023440b51d7ed86c09aff17f.jpg'),
+	(31,'Downstairs J','Basement, etc...','Insienso','2021',3,'39eef8d060125aa7c2783dfeacec7e846e24b101.jpg'),
+	(32,'Various','Virtual Dreams (Ambient Explorations In The House & Techno Age, 1993-1997)','Music From Memory','2020',5,'72971f6cbe19c87baf90cbe4f56b3bb6dbbdc88f.jpg'),
+	(33,'Dbridge & Vegas','True Romance VIP / Inner Disbelief VIP','Metalheadz','2014',2,'3783b32e9392921401ba98a32c44657a4e969d86.jpg');
 
 /*!40000 ALTER TABLE `releases` ENABLE KEYS */;
 UNLOCK TABLES;
